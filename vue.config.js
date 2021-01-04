1828068122577f051490804b53df9752ad8b7ea6
yjf <1294485765@qq.com>
2021-01-04 11:22:52 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
+  outputDir: 'docs',
   productionSourceMap: process.env.NODE_ENV === "production" ? false : true,
   transpileDependencies: [
     "vuetify"
